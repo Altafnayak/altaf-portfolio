dbdbee64d6303f503b88b33d847125f4e42cf413
Altaf Nayak <altafn899@gmail.com>
2026-05-24 22:58:36 +0530

    feat(hero): add resume download link and security attributes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ function Hero() {
         </p>
 
         <div className="hero-buttons">
-          <a href="/resume.pdf" className="btn btn-primary" target="_blank">
+          <a href="https://drive.google.com/file/d/15P5eYdKduIof7UptykKG1jqyhVTGYlvk/view?usp=sharing" className="btn btn-primary" target="_blank">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
             DOWNLOAD RESUME
           </a>
